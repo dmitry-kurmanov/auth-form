17bb25aa048dc653912addb146745d045264ac0c
dmitry-kurmanov <kurmanov.work@gmail.com>
2024-09-11 15:26:47 +0400

    started with react way states

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,64 +1,78 @@
+import { MutableRefObject, useRef, useState } from "react";
 import Input from "../input/Input";
 
 export default function Form() {
-  const validationCheck = (): boolean => {
-    const email = document.getElementById("email") as HTMLInputElement;
-    const password = document.getElementById("password") as HTMLInputElement;
+  const emailRef: MutableRefObject<HTMLInputElement | null> = useRef(null);
+  const passwordRef: MutableRefObject<HTMLInputElement | null> = useRef(null);
 
-    email.classList.remove("login-form__input--invalid");
-    password.classList.remove("login-form__input--invalid");
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
+  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
 
-    let result = true;
+  const validationCheck = () => {
+    const email = emailRef.current;
+    const password = passwordRef.current;
 
-    //TODO refactoring
-    const emailErrorMessage = document.getElementById("email-error-message");
-    if (email.checkValidity()) {
-      emailErrorMessage!.innerText = "";
-      email.setAttribute("aria-invalid", "false");
-      email.removeAttribute("aria-describedby");
-      email.classList.remove("login-form__input--invalid");
-      const passwordErrorMessage = document.getElementById(
-        "password-error-message"
-      );
-      if (password.checkValidity()) {
-        passwordErrorMessage!.innerText = "";
-        password.setAttribute("aria-invalid", "false");
-        password.removeAttribute("aria-describedby");
-        password.classList.remove("login-form__input--invalid");
-      } else {
-        passwordErrorMessage!.innerText =
-          "Error in Password Field: " + password.validationMessage;
-        password.setAttribute("aria-invalid", "true");
-        password.setAttribute("aria-describedby", "password-error-message");
-        password.classList.add("login-form__input--invalid");
-        password.focus();
-        result = false;
-      }
-    } else {
-      emailErrorMessage!.innerText =
-        "Error in Email Field: " + email.validationMessage;
-      email.setAttribute("aria-invalid", "true");
-      email.setAttribute("aria-describedby", "email-error-message");
-      email.classList.add("login-form__input--invalid");
-      email.focus();
-      result = false;
-    }
-    // EO TODO refactoring
+    if (!email || !password) return;
 
-    return result;
+    setIsEmailValid(email.checkValidity());
+    setIsPasswordValid(password.checkValidity());
+
+    // //
+
+    // email.classList.remove("login-form__input--invalid");
+    // password.classList.remove("login-form__input--invalid");
+
+    // let result = true;
+
+    // //TODO refactoring
+    // const emailErrorMessage = document.getElementById("email-error-message");
+    // if (email.checkValidity()) {
+    //   emailErrorMessage!.innerText = "";
+    //   email.setAttribute("aria-invalid", "false");
+    //   email.removeAttribute("aria-describedby");
+    //   email.classList.remove("login-form__input--invalid");
+    //   const passwordErrorMessage = document.getElementById(
+    //     "password-error-message"
+    //   );
+    //   if (password.checkValidity()) {
+    //     passwordErrorMessage!.innerText = "";
+    //     password.setAttribute("aria-invalid", "false");
+    //     password.removeAttribute("aria-describedby");
+    //     password.classList.remove("login-form__input--invalid");
+    //   } else {
+    //     passwordErrorMessage!.innerText =
+    //       "Error in Password Field: " + password.validationMessage;
+    //     password.setAttribute("aria-invalid", "true");
+    //     password.setAttribute("aria-describedby", "password-error-message");
+    //     password.classList.add("login-form__input--invalid");
+    //     password.focus();
+    //     result = false;
+    //   }
+    // } else {
+    //   emailErrorMessage!.innerText =
+    //     "Error in Email Field: " + email.validationMessage;
+    //   email.setAttribute("aria-invalid", "true");
+    //   email.setAttribute("aria-describedby", "email-error-message");
+    //   email.classList.add("login-form__input--invalid");
+    //   email.focus();
+    //   result = false;
+    // }
+    // // EO TODO refactoring
+
+    // return result;
   };
 
   const handleValidation = () => {
-    const isValid = validationCheck(); // return invalid field ID instead of boolean
+    validationCheck(); // return invalid field ID instead of boolean
 
     // (document.getElementById("password") as HTMLInputElement).value = "";
-    if (isValid) {
+    if (isEmailValid && isPasswordValid) {
       //sendToserver();
       //TODO if server response error
-      document
-        .getElementById("serverMessage")
-        ?.classList.toggle("login-form__server-message--hidden");
-      (document.getElementById("email") as HTMLInputElement).focus();
+      // document
+      //   .getElementById("serverMessage")
+      //   ?.classList.toggle("login-form__server-message--hidden");
+      // (document.getElementById("email") as HTMLInputElement).focus();
       //TODO if server response ok
       // ... setState(loggedUser), setState("show success block")
     }
@@ -96,12 +110,21 @@ export default function Form() {
             >
               Something went wrong...
             </div>
-            <Input id="email" type="email" isRequred={true} labelText="Email" />
+            <Input
+              id="email"
+              type="email"
+              isRequred={true}
+              labelText="Email"
+              ref={emailRef}
+              isValid={isEmailValid}
+            />
             <Input
               id="password"
               type="password"
               isRequred={true}
               labelText="Password"
+              ref={passwordRef}
+              isValid={isPasswordValid}
             />
           </div>
 
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,35 +1,52 @@
-interface IInputParameters {
+import {
+  ForwardedRef,
+  MutableRefObject,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+export interface IInputParameters {
   id: string;
   type: string;
   isRequred: boolean;
   labelText: string;
+  isValid?: boolean;
 }
 
-export default function Input({
-  id,
-  type,
-  isRequred,
-  labelText,
-}: IInputParameters) {
+const Input = forwardRef(function Input(
+  { id, type, isRequred, labelText, isValid = true }: IInputParameters,
+  forwardedRef: ForwardedRef<HTMLInputElement>
+) {
+  const errorMessageId = `${id}-error-message`;
+
+  const ref: MutableRefObject<HTMLInputElement | null> = useRef(null);
+  useImperativeHandle(forwardedRef, () => ref.current as HTMLInputElement);
+
   return (
     <div className="login-form__input-wrapper">
       <input
         id={id}
         type={type}
         required={isRequred}
-        className="login-form__input"
-        aria-invalid="false"
+        className={`login-form__input${
+          isValid ? "" : " login-form__input--invalid"
+        }`}
+        aria-invalid={isValid}
+        aria-describedby={isValid ? undefined : errorMessageId}
         placeholder=""
         /* aria-errormessage is better than aria-describedby but unfortunatelly is not fully supported https://stackoverflow.com/a/78675883/6623551 */
         /*aria-errormessage="email-error-message"*/
+        ref={ref}
       ></input>
       <label className="login-form__input-label" htmlFor={id}>
         {labelText}
       </label>
-      <span
-        id={`${id}-error-message`}
-        className="login-form__input-error-message"
-      ></span>
+      <span id={errorMessageId} className="login-form__input-error-message">
+        {ref.current?.validationMessage}
+      </span>
     </div>
   );
-}
+});
+
+export default Input;
